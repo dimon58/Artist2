0a06c92f7668aca4680932a241b7353918ea430f
Dmitry <dimansharonov95@gmail.com>
2021-12-24 18:08:18 +0300

    Добавлено рисование многих картинок сразу

--- handlers/rudalle.py
+++ handlers/rudalle.py
@@ -47,3 +47,45 @@ async def draw(message: types.Message):
     ###################################################################################################################
 
     await send_photo(message, upscaled_image, image, 'result.png')
+
+
+@dp.message_handler(commands=['drawm', 'dm'])
+async def drawm(message: types.Message):
+    logger.info(f'Draw many "{message.text}" request by {message.from_user.id}')
+
+    if len(message.text) <= 6:
+        return await message.answer("Напишите описание изображения. /draw *описание*")
+
+    _, num, *text = message.text.split()
+    text = ' '.join(text)
+
+    ###################################################################################################################
+
+    await message.answer(f'Начинаю рисовать "{text}"')
+
+    images = generate(text, images_num=num, chat_id=message.chat.id)
+
+    if not settings.RUDALLE_SUPERRESOLUTION:
+        for image in images:
+            await message.answer_photo(pil2tg(image))
+        return
+
+    await message.answer(f'Улучшение качества')
+
+    ###################################################################################################################
+
+    for image in images:
+        start = time.perf_counter()
+        upscaled_image = upscale_pil(
+            image,
+            model_name=settings.RUDALLE_REALESRGAN_MODEL_UPSCALER,
+            tile=settings.REALESRGAN_TILES,
+            face_enhance=True,
+            half=settings.HAFT_PRECISION
+        )
+        end = time.perf_counter()
+        print(f'{upscaled_image.width // 4}x{upscaled_image.height // 4} upscaled in {end - start:.4f} sec')
+
+        ###############################################################################################################
+
+        await send_photo(message, upscaled_image, image, 'result.png')
